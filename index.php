6caefbc0ee057e9e6e2236c336ff20ce51220dfd
GiuseppeCerto <116110501+GiuseppeCerto@users.noreply.github.com>
2023-03-20 16:30:58 +0100

    EDIT table for hotels items

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title>HOTEL</title>
 </head>
 <body>
@@ -49,18 +50,35 @@
 
         ];
 
-        
-    ?>
-
-    <?php foreach ($key as $hotel) { ?>
-        <?php echo $hotel['name']; ?>
-        <?php echo $hotel['name']; ?>
-    <?php } ?>
-
-
-
-
-
-
+        ?>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Description</th>
+                                <th>Parking</th>
+                                <th>Vote</th>
+                                <th>Distance to center</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <?php foreach ($hotels as $hotel) { ?>
+                                    <td><?php echo $hotel['name']; ?></td>
+                                    <td><?php echo $hotel['description']; ?></td>
+                                    <td><?php echo ($hotel['parking'] ? 'Si' : 'No'); ?></td>
+                                    <td><?php echo $hotel['vote']; ?></td>
+                                    <td><?php echo $hotel['distance_to_center']; ?> km</td>
+                                <?php } ?>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+            
 </body>
 </html>
